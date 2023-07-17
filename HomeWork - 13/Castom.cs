1f188b0806de7aa7513e5a50faec57b0cae2f943
DokermanD <Dmitry.Hatkevich@gmail.com>
2023-07-17 04:04:27 +0300

    Добавьте файлы проекта.

---
+++ HomeWork - 13/Castom.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+
+namespace HomeWork___13
+{
+    public class Castom
+    {
+
+        public static string? Serial { get; set; }
+        public static string? DeSerial { get; set; }
+
+        /// <summary>
+        /// Сериализация объекта 
+        /// </summary>
+        /// <param name="obj"> объект для сериализации </param>
+        public static void Ser(object obj)
+        {
+            //Получаем тип объекта
+            var type = obj.GetType();
+            //Получаем поля объекта с данными
+            var filds = type.GetFields();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var fild in filds)
+            {
+                sb.Append($"{fild.Name},{fild.GetValue(obj)}\n");
+            }
+            Serial = sb.ToString();
+
+            using FileStream fs = new FileStream(@"C:\Users\dmitr\Desktop\Test.csv", FileMode.Create);
+            using StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(sb);
+
+            sb.Clear();
+        }
+
+        /// <summary>
+        /// Десереализация объекта
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static void DeSer()
+        {
+            F f2 = new F();
+            //Получаем тип объекта
+            var type = f2.GetType();
+            //Получаем поля объекта с данными
+            var filds = type.GetFields();
+            //Читаем строку из файла
+            var arrayStr = File.ReadAllLines(@"C:\Users\dmitr\Desktop\Test.csv");
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var fild in filds)
+            {
+                fild.SetValue(f2, GetValueFile(fild, arrayStr));
+                sb.Append($"Name - {fild.Name} Value - {fild.GetValue(f2).ToString()}\n");
+            }
+            DeSerial = sb.ToString();
+
+        }
+
+        /// <summary>
+        /// Поиск в строке по имени поля его значения
+        /// </summary>
+        /// <param name="field">объект</param>
+        /// <param name="arrayStr">строка с данными после сериализации</param>
+        /// <returns></returns>
+        public static int GetValueFile(System.Reflection.FieldInfo field, string[] arrayStr)
+        {
+            string value = string.Empty;
+            var rezultIntValue = arrayStr.Where(x => x.Contains(field.Name));
+            foreach (var rezult in rezultIntValue)
+            {
+                value = rezult.Split(',')[1];
+            }
+            return Convert.ToInt32(value);
+        }
+    }
+}
---
+++ HomeWork - 13/Program.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Threading.Channels;
+
+namespace HomeWork___13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch swSer = new Stopwatch();
+            
+            F f = new F();
+
+            //Сереализация объекта
+            Console.WriteLine("Сереализация объекта");
+            swSer.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                Castom.Ser(f.Get()); 
+            }
+            swSer.Stop();
+            Console.WriteLine($"Затрачено времени на 1000 итераций - {swSer.ElapsedMilliseconds.ToString()}\n{Castom.Serial}\n");
+
+            //Десереализация объекта
+            Console.WriteLine("Десереализация объекта");
+            swSer.Restart();
+            for (int i = 0; i < 1000; i++)
+            {
+                Castom.DeSer(); 
+            }
+            swSer.Stop();
+            Console.WriteLine($"Затрачено времени на 1000 итераций - {swSer.ElapsedMilliseconds.ToString()}\n{Castom.DeSerial}\n");
+
+
+            //------------------------------------------------------------------------------------------------//
+
+            string stringSerialize = string.Empty;
+            var v = f.Get();
+
+            //Сереализация объекта (Newtonsoft.Json)
+            Console.WriteLine("Сереализация объекта (Newtonsoft.Json)");
+            swSer.Restart();
+            for (int i = 0; i < 1000; i++)
+            {   
+               stringSerialize = JsonConvert.SerializeObject(v);
+            }
+            swSer.Stop();
+            Console.WriteLine($"Затрачено времени на 1000 итераций - {swSer.ElapsedMilliseconds.ToString()}\n{stringSerialize}\n");
+
+            F? deser = new F();
+            //Десереализация объекта  (Newtonsoft.Json)
+            Console.WriteLine("Десереализация объекта (Newtonsoft.Json)");
+            swSer.Restart();
+            for (int i = 0; i < 1000; i++)
+            {
+                deser = JsonConvert.DeserializeObject<F>(stringSerialize);
+            }
+            swSer.Stop();
+            Console.WriteLine($"Затрачено времени на 1000 итераций - {swSer.ElapsedMilliseconds.ToString()}\n");
+            var deserType = deser?.GetType();
+            var deserFilds = deserType?.GetFields();
+            foreach (var item in deserFilds)
+            {
+                Console.WriteLine($"{item.Name}:{item.GetValue(deser)}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
